ee745199a11b07d25b1d27a6bceded2737bec301
mansi2020 <sorathiyamansi11@gmail.com>
2023-12-24 19:31:43 +0530

    add all functionality and style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Main from "./Component/Main";
+import Image from "./Image.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App_container">
+      <h1>Welcome to Password Generator!</h1>
+      <div className="App_container_sub">
+      <img src={Image} alt="" />
+        <Main></Main>
+      </div>
     </div>
   );
 }
